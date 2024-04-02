916e8f618a48742bfabe8dab3c530b854ca89680
Diego Gonzalez <degonzalez6@gmail.com>
2024-04-02 15:02:18 -0300

    Primer commit

---
+++ gestionPedidos/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from gestionPedidos.models import Articulos
+# Create your views here.
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+
+    if request.GET["prd"]:
+        producto=request.GET["prd"]
+
+        if len(producto)>20:
+            mensaje="Texto de búsqueda demasiado largo"
+        else: 
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
+            return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+    else:
+        mensaje="No has introducido nada"
+    return HttpResponse(mensaje)
+
+def contacto(request):
+    return render(request, "contacto.html")
