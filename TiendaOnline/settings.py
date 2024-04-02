79a8ae8b6bf4ba87a32ec3c3204131b9defad543
Diego Gonzalez <degonzalez6@gmail.com>
2024-04-02 16:23:08 -0300

    Configuración de envío de email, falta configurar gmail

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -134,9 +134,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackEnd"
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="degonzalez6"
+EMAIL_HOST_USER=""
 EMAIL_HOST_PASSWORD=""
