d4f3031ab645ade031a44fb10a1f193078bc2f9d
Diego Gonzalez <degonzalez6@gmail.com>
2024-04-02 16:04:54 -0300

    Agregando conf de email en settings.py

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -133,3 +133,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackEnd"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="degonzalez6"
+EMAIL_HOST_PASSWORD=""
